b4384beeeea7446a0184a451f2e1f7f2b9ffe2dc
marcio30peres <marcio30peres@gmail.com>
2021-03-10 11:51:34 -0300

    Desafio concluído

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,29 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle !== '') {
+          const newTask: Task = {
+            id: Math.round(Math.random() * 1000),
+            title: newTaskTitle,
+            isComplete: false
+          }
+      
+          setTasks([...tasks, newTask])
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task))
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
